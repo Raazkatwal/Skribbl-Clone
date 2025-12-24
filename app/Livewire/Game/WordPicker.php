31b9372a51af89901d961b14d7a070b54b0614d9
Raazkatwal <raazkatwal5@gmail.com>
2025-12-24 19:24:09 +0545

    wip: Refactor - Changing file structure

---
+++ app/Livewire/Game/WordPicker.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Game;
+
+use Livewire\Component;
+
+class WordPicker extends Component
+{
+    public function render()
+    {
+        return view('livewire.game.word-picker');
+    }
+}
